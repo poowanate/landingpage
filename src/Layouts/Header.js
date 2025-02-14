9f1c78f9a90904c002bace567716731ae6238882
poowanate <poowanateza@hotmail.com>
2025-02-15 01:22:35 +0700

    home

---
+++ src/Layouts/Header.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import '../css/header.css'
+import '../css/main.css'
+
+const Header = () => {
+  return (
+    <div className=''>
+    <header>
+        
+        <div class="icons disabledesktop">
+           
+        <a href="#">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+</svg>
+
+</a>
+      
+            <a href="#" class="cart">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+</svg>
+
+                <span>0</span>
+            </a>
+        </div>
+        <div class="logo">OWNDAYS</div>
+       
+        <nav>
+            <a href="#">搜尋眼鏡</a>
+            <a href="#">隱形眼鏡</a>
+            <a href="#">品牌</a>
+            <a href="#">鏡片</a>
+            <a href="#">關於OWNDAYS</a>
+            <a href="#">門市查詢</a>
+        </nav>
+        <div class="icons disable">
+            <a className='' href="#">登入</a>
+            <a href="#">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+</svg>
+
+</a>
+            <a href="#">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+</svg>
+</a>
+            <a href="#" class="cart">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+</svg>
+
+                <span>0</span>
+            </a>
+        </div>
+        <div className='disabledesktop'>
+
+            <a className=''><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/>
+</svg>
+</a>
+
+<div class=" ml16 burger" onclick="toggleMenu()">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        </div>
+    </header>
+    <hr/>
+    <p style={{ 'font-size':'12px', 'padding-left':'32px'}}><a  className='hover'> OWNDAYS眼鏡公司首頁</a> > <a  className='hover'>特集</a> > <a  className='hover'>OWNDAYS × NORITAKE </a></p>
+    
+    <hr/>
+   
+    </div>
+
+  )
+  
+}
+
+export default Header
