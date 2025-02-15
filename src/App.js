856e5515741e9d455e09631798bce66837350152
poowanate <poowanateza@hotmail.com>
2025-02-15 20:50:27 +0700

    add image Novelty

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Layouts/Header';
-import Home from './components/Home';
-import Story from './components/Story';
-import Novelty from './components/Novelty';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Layouts/Header";
+import Home from "./components/Home";
+import Story from "./components/Story";
+import Novelty from "./components/Novelty";
 
 function App() {
   return (
     <>
-      <Header/>
-    <div className='pd2'>
-    <Home/>
-    <Story/>
-    <Novelty/>
-    </div>
-    
+      <Header />
+      <div className="pd2">
+        <Home />
+        <Story />
+        <Novelty />
+      </div>
     </>
   );
 }
--- src/components/Novelty.js
+++ src/components/Novelty.js
@@ -1,44 +1,144 @@
-import React from 'react'
-import '../css/novelty.css'
+import React from "react";
+import "../css/novelty.css";
 
 const Novelty = () => {
   return (
-    <div> <h2>NOVELTY</h2>
-    <div class="highlight">全門市滿額限定好禮</div>
-    <div class="gridnovelty">
-       
-        <div class="item">
-            <p class="date">2023.12.1</p>
-            <img src="https://placehold.co/200x200" alt="2023.12.1 年曆海報" />
-            <p>送 年曆海報</p>
-            <p>消費不限金額</p>
-        </div>
-        
-        <div class="item">
-            <p class="date">2024.1.1</p>
-            <img src="https://placehold.co/200x200" alt="2024.1.1 限量眼鏡布組" />
-            <p>送 限量眼鏡布組</p>
-            <p>滿2,990元</p>
-        </div>
-      
-        <div class="item">
-            <p class="date">2024.2.1</p>
-            <img src="https://placehold.co/200x200" alt="2024.2.1 圓枕枕" />
-            <p>送 圓枕枕</p>
-            <p>滿4,290元</p>
-        </div>
-       
-        <div class="item">
-            <p class="date">2024.2.1</p>
-            <img src="https://placehold.co/200x200" alt="2024.2.1 圓枕枕" />
-            <p>送 圓枕枕</p>
-            <p>滿4,290元</p>
-        </div>
-      
-    </div>
-    </div>
-   
-  )
-}
+    <div className="page">
+      <div
+        className="story-container titlenovelty "
+        style={{ placeItems: "center" }}
+      >
+        NOVELTY
 
-export default Novelty
+      </div>
+      
+      <div class="highlight dot-highlight">
+        <span className="dot">全門市滿額限定好禮</span>
+      </div>
+      <div class="gridnovelty body-content container">
+        <div class="item ">
+          <div className="pd2">
+            <span class="date datail row">2023.12.1 <span className="dot-highlightnov-black">五</span> <div className="arrow"></div></span>
+
+            <div class="card mt-25 detail">
+              <div className="card1"></div>
+            </div>
+
+            <p className="mt-25 detail">
+              {" "}
+              <span className=" dot-highlightnov">送</span> 年曆海報
+            </p>
+            <button className="button-black-nov paddingLR">消費不限金額</button>
+          </div>
+        </div>
+
+        <div class="item ">
+          <div className="pd2">
+            <span class="date datail row">2024.1.<span className="dot-highlightnov-black">一</span> <div className="arrow"></div></span>
+
+            <div class="card mt-25 detail">
+              <div className="card2"></div>
+            </div>
+
+            <p className="mt-25 detail">
+              {" "}
+              <span className=" dot-highlightnov">送</span> 眼鏡盒布組
+            </p>
+            <button className="button-black-nov paddingLR">滿2,990元</button>
+          </div>
+        </div>
+        <div class="item ">
+          <div className="pd2">
+            <span class="date datail row">2024.2.<span className="dot-highlightnov-black">四</span> <div className="arrow"></div></span>
+
+            <div class="card mt-25 detail">
+              <div className="card3"></div>
+            </div>
+
+            <p className="mt-25 detail">
+              {" "}
+              <span className=" dot-highlightnov">送</span> 圓抱枕
+            </p>
+            <button className="button-black-nov paddingLR">滿4,290元</button>
+          </div>
+        </div>
+        <div class="item ">
+          <div className="pd2">
+            <span class="date datail row">2024.3.<span className="dot-highlightnov-black">五</span> <div className="arrow"></div></span>
+
+            <div class="card mt-25 detail">
+              <div className="card4"></div>
+            </div>
+
+            <p className="mt-25 detail">
+              {" "}
+              <span className=" dot-highlightnov">送</span> 吸管杯
+            </p>
+            <button className="button-black-nov paddingLR">滿3,490元</button>
+          </div>
+        </div>
+        <div class="item ">
+          <div className="pd2">
+            <span class="date datail row">2024.4.<span className="dot-highlightnov-black">一</span> <div className="arrow"></div></span>
+
+            <div class="card mt-25 detail">
+              <div className="card5"></div>
+            </div>
+
+            <p className="mt-25 detail">
+              {" "}
+              <span className=" dot-highlightnov">送</span> 筆記本
+            </p>
+            <button className="button-black-nov paddingLR">滿3,490元</button>
+          </div>
+        </div>
+        <div class="item ">
+          <div className="pd2">
+            <span class="date datail row">2024.5.<span className="dot-highlightnov-black">三</span> <div className="arrow"></div></span>
+
+            <div class="card mt-25 detail">
+              <div className="card6"></div>
+            </div>
+
+            <p className="mt-25 detail">
+              {" "}
+              <span className=" dot-highlightnov">送</span> 托特包
+            </p>
+            <button className="button-black-nov paddingLR">滿3,490元</button>
+          </div>
+        </div>
+      </div>
+      <div className="middle py-10">
+      <button className="buttonrounded ">
+          <svg
+            width="35"
+            height="35"
+            viewBox="0 0 35 35"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="17.5" cy="17.5" r="17.5" fill="black" />
+            <line
+              x1="14.7178"
+              y1="10.2824"
+              x2="21.0818"
+              y2="16.6464"
+              stroke="white"
+            />
+            <line
+              x1="14.0107"
+              y1="23.0105"
+              x2="20.3747"
+              y2="16.6465"
+              stroke="white"
+            />
+          </svg>
+          <span>前往OWNDAYS官方分店</span>{" "}
+        </button>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Novelty;
